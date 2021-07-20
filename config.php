1fd808be3a4551ce771c5b4016c81026fa5eb231
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2021-07-20 16:39:24 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,3 +4,6 @@ $sDBHostLAB = 'localhost';
 $sDBNameLAB = 'dbname';
 $sDBUsrLAB = 'dbuser';
 $sDBPswLAB = 'dbpsw';
+
+//Directorio donde se encuentran los archivos SLD
+define('ESTILOS-DIR', '../estilos-sld/');
